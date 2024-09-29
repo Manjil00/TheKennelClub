8816bafcc01be2547d567c36137256941e828699
ACER <210220@softwarica.edu.np>
2024-09-29 20:20:53 +0545

    routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Landing from './pages/Landing';
+import Login from './pages/Login';
 
 function App() {
 
 return (
 <BrowserRouter>
 <Routes>
-      <Route path="/" element={<Home/>} />
-      
+      <Route path="/" element={<Landing/>} />
+      <Route path="/login" element={<Login/>} />
     </Routes>
 </BrowserRouter>
   )
