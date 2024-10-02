569e1da23e49b45a33f485f933991c90fb84170c
ACER <210220@softwarica.edu.np>
2024-10-02 20:23:57 +0545

    nav bar

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,10 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { RxCross1, RxHamburgerMenu } from "react-icons/rx"; //ICONS
+import { Link } from 'react-router-dom';
+
 
 const Nav = () => {
-return (
-<div>
+    const [isNavOpen, setIsNavOpen] = useState(false);
+const toggleNavMenu = () => setIsNavOpen(!isNavOpen);
 
-</div>
+return (
+    <nav className="flex justify-between px-6 py-3 z-50 relative">
+    <div className="flex justify-center items-center">
+        <img className="w-10 h-10 mr-3" src="./images/GN_logo.png" alt="Logo" />
+        <h1 className='text-black ml-6  text-sm md:text-4xl font-LogoFont
+cursor-pointer flex justify-center items-center'>Gajendra Nursery</h1>
+
+    </div>
+    <div className={`bg-white absolute min-h-[30vh] left-0 top-[90%] w-full p-5 flex flex-col md:static md:flex md:min-h-fit md:w-auto gap-9 text-sm ${isNavOpen ? 'block' : 'hidden'}`}>
+        <ul className=" cursor-pointer flex flex-col gap-4 md:flex-row  md:gap-8 md:justify-evenly md:text-xl font-writingFont">
+        <Link to="/"><li className="hover:text-green-600">Home</li></Link>
+     
+
+        </ul>
+    </div>
+    <div>
+        <button onClick={toggleNavMenu} className="md:hidden">
+            {
+            isNavOpen ? <RxCross1 className="text-2xl text-black" /> :
+            <RxHamburgerMenu className={`text-2xl text-black  ${isNavOpen ? 'hidden' : 'block'}`} />
+            }
+        </button>
+    </div>
+</nav>
 )
 }
 
