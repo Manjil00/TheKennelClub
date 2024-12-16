8f53b4017959ec994da47c044a0d4afa4361c789
ACER <210220@softwarica.edu.np>
2024-12-16 23:36:41 +0545

    nameschange

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -12,7 +12,7 @@ return (
     <div className="flex justify-center items-center">
         <img className="w-10 h-10 mr-3" src="./images/GN_logo.png" alt="Logo" />
         <h1 className='text-black ml-6  text-sm md:text-4xl font-LogoFont
-cursor-pointer flex justify-center items-center'>Gajendra Nursery</h1>
+cursor-pointer flex justify-center items-center'>The Kennel Club</h1>
 
     </div>
     <div className={`bg-white absolute min-h-[30vh] left-0 top-[90%] w-full p-5 flex flex-col md:static md:flex md:min-h-fit md:w-auto gap-9 text-sm ${isNavOpen ? 'block' : 'hidden'}`}>
