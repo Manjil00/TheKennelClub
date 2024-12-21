4ae4a7a1c8f87a3fb8f37c2e73c747caf8f13260
ACER <210220@softwarica.edu.np>
2024-12-21 23:52:50 +0545

    f

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import Nav from "../components/Nav";
+import Footer from '../components/Footer';
+
 
 const Home = () => {
 return (
     <div className='main-container h-full w-full'>
         <Nav/>
+
+
+        <Footer/>
     </div>
 )
 }
