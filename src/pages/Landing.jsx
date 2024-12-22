df64ec990d47f4f9b1ccda36c00a3035aae5ba64
ACER <210220@softwarica.edu.np>
2024-12-23 00:04:08 +0545

    b-2

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import LandingNav from '../components/LandingNav';
+
 
 const Landing = () => {
   return (
-    <div className='w-full h-[100vh] bg-teal-500'>
-      
+    <div className='w-full h-[100vh]'>
+      <LandingNav/>
     </div>
   )
 }
