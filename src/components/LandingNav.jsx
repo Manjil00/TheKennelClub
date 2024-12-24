86198442aa3a30d907befa172c32f2e416855ef5
ACER <210220@softwarica.edu.np>
2024-12-25 00:06:50 +0545

    correction

--- src/components/LandingNav.jsx
+++ src/components/LandingNav.jsx
@@ -13,10 +13,10 @@ return (
         <img className="w-10 h-10 mr-3" src="/src/assets/logo.png" alt="Logo" />
         <h1 className='text-black ml-6  text-sm md:text-2xl
 cursor-pointer flex justify-center items-center'>The Kennel Club</h1>
-
     </div>
+
     <div className={`bg-white absolute min-h-[30vh] left-0 top-[90%] w-full p-5 flex flex-col md:static md:flex md:min-h-fit md:w-auto gap-9 text-sm ${isNavOpen ? 'block' : 'hidden'}`}>
-        <ul className=" cursor-pointer flex flex-col gap-4 md:flex-row  md:gap-8 md:justify-evenly md:text-xl font-writingFont">
+        <ul className=" cursor-pointer flex flex-col gap-4 md:flex-row  md:gap-8 md:justify-evenly md:text-xl ">
         <Link to="/login"><li className="border-2 border-purple-600 p-2 rounded-lg hover:text-purple-600">Login</li></Link>
         <Link to="/register"><li className="border-2 border-purple-600 p-2 rounded-lg hover:text-purple-600">Register</li></Link>
         </ul>
