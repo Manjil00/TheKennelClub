c2113d2bbafb59600a43916a2dd16555c1e2b613
ACER <210220@softwarica.edu.np>
2024-12-29 00:16:27 +0545

    change

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Landing from './pages/Landing';
+// import Landing from './pages/Landing';
 import Login from './pages/Login';
 import Register from './pages/Register';
-// import Home from './pages/Home';
+import Home from './pages/Home';
 
 function App() {
 return (
 <BrowserRouter>
 <Routes>
-      <Route path="/" element={<Landing/>} />
-      {/* <Route path="/" element={<Home/>} /> */}
+      {/* <Route path="/" element={<Landing/>} /> */}
+      <Route path="/" element={<Home/>} />
       <Route path="/login" element={<Login/>} />
       <Route path="/register" element={<Register/>} />
     </Routes>
