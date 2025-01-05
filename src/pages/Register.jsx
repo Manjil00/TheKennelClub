3e37cc00ff33bc4ae61652e8e296248e6ea254ad
ACER <210220@softwarica.edu.np>
2025-01-05 13:36:46 +0545

    landingnav

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import LandingNav from '../components/LandingNav';
 
 const Register = () => {
 return (
 <div className='mainContainer w-full h-[100vh]'>
-    
+    <LandingNav/>
 </div>
 )
 }
