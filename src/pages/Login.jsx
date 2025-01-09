a114bd9b70cf225beba359eba9d01444fd3d1628
ACER <210220@softwarica.edu.np>
2025-01-09 14:20:33 +0545

    sections

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -3,16 +3,19 @@ import LandingNav from '../components/LandingNav';
 
 const Login = () => {
   return (
-    <div className='mainContainer w-full h-[100vh] px-10 py-3'>
+    <div className='mainContainer w-full h-[100vh] '>
       <LandingNav/>
 
+
+  <div className="InnerContainer w-full h-auto px-20 py-4 flex justify-evenly items-center gap-10">
       <div className="leftSection w-[50%] h-[200px] bg-red-800">
 
       </div>
 
-      <div className="rightSection w-[50%] h-[200px] bg-red-800">
+  <div className="rightSection w-[50%] h-[200px] bg-green-800">
 
-      </div>
+  </div>
+</div>
 
     </div>
   )
