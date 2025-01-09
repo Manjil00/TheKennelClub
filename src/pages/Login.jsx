435f3a84957ec40c942d5455f879710e795e01c6
ACER <210220@softwarica.edu.np>
2025-01-09 14:15:53 +0545

    seclanding

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -3,8 +3,17 @@ import LandingNav from '../components/LandingNav';
 
 const Login = () => {
   return (
-    <div className='mainContainer w-full h-[100vh]'>
+    <div className='mainContainer w-full h-[100vh] px-10 py-3'>
       <LandingNav/>
+
+      <div className="leftSection w-[50%] h-[200px] bg-red-800">
+
+      </div>
+
+      <div className="rightSection w-[50%] h-[200px] bg-red-800">
+
+      </div>
+
     </div>
   )
 }
