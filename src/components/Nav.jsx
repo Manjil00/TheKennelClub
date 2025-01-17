615aa708d33de66ff63f519edee22fdf520a5a32
ACER <210220@softwarica.edu.np>
2025-01-17 12:32:13 +0545

    transpar nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -8,14 +8,14 @@ const Nav = () => {
 const toggleNavMenu = () => setIsNavOpen(!isNavOpen);
 
 return (
-    <nav className="flex justify-between px-6 py-3 z-50 relative">
-    <div className="flex justify-center items-center">
+    <nav className="flex justify-between px-6 py-3 z-50 relative bg-red-800">
+    <div className="flex justify-start items-center">
         <img className="w-10 h-10 mr-3" src="/src/assets/logo.png" alt="Logo" />
         <h1 className='text-black ml-6  text-sm md:text-2xl
 cursor-pointer flex justify-center items-center'>The Kennel Club</h1>
-
     </div>
-    <div className={`bg-white absolute min-h-[30vh] left-0 top-[90%] w-full p-5 flex flex-col md:static md:flex md:min-h-fit md:w-auto gap-9 text-sm ${isNavOpen ? 'block' : 'hidden'}`}>
+    
+    <div className={`bg-white md:bg-transparent absolute min-h-[30vh] left-0 top-[90%] w-full p-5 flex flex-col  md:static md:flex md:min-h-fit md:w-auto gap-9 text-sm ${isNavOpen ? 'block' : 'hidden'}`}>
         <ul className=" cursor-pointer flex flex-col gap-4 md:flex-row  md:gap-8 md:justify-evenly md:text-xl font-writingFont">
         <Link to="/"><li className="hover:text-purple-800">Home</li></Link>
         <Link to="#"><li className="hover:text-purple-800">Appointment</li></Link>
