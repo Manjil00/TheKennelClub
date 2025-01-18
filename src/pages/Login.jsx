4e93e3b81a728e04e9da5c2d618714891a2ed6fc
ACER <210220@softwarica.edu.np>
2025-01-18 13:39:51 +0545

    loginregister

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
 import LandingNav from '../components/LandingNav';
 import LeftPatch from '../components/LeftPatch';
-
 const Login = () => {
   return (
     <div className='mainContainer w-full h-auto'>
@@ -11,7 +12,7 @@ const Login = () => {
     
     <LeftPatch/>
 
-  <div className="rightPatch w-[90%]  md:w-[30%] h-[400px] md:h-[500px]  rounded-xl  border-2 border-slate-400 flex justify-center items-center">
+  <div className="rightPatch py-3 w-[90%]  md:w-[30%] h-[400px] md:h-[500px]  rounded-xl  border-2 border-slate-400 flex justify-center items-center">
         
   <div className="inputscontainer  w-[300px] h-full flex flex-col justify-center items-center gap-5">
         <button className="bg-blue-700 text-white font-bold p-1 w-full h-[40px] rounded-lg flex justify-center items-center gap-10">
@@ -22,7 +23,7 @@ const Login = () => {
         <h1 className='text-slate-500 font-bold'>OR</h1>
 
         <div className="inputs  text-black flex flex-col gap-5 w-full">
-        <input className='w-full h-[40px] p-5 rounded-xl  placeholder-slate-500 border-slate-300 border-2' placeholder='Username'/>
+        <input className='w-full h-[40px] p-5 rounded-xl  placeholder-slate-500 border-slate-300 border-2' placeholder='Phone Number'/>
         <input className='w-full h-[40px] p-5 rounded-xl placeholder-slate-500  border-slate-300 border-2' placeholder='Password'/>
         </div>
         
@@ -30,12 +31,12 @@ const Login = () => {
 
         <button className='mx-auto bg-purple-700 w-full h-[40px] rounded-lg text-white'>Get started for free</button>
 
-        <div className="label text-sm">Don't have an account? <span className='underline text-blue-700 cursor-pointer'>Register</span></div>
-  </div>
-        
+        <div className="label text-sm">Don't have an account? <span className='underline text-blue-700 cursor-pointer'><Link to="/register">Register</Link></span></div>
 
-        
+        <div className="label text-xs underline text-blue-700 cursor-pointer w-full text-end"><Link to="/register">Forgot Password?</Link></div>
   </div>
+  </div>
+
 </div>
 
     </div>
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,10 +1,42 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 import LandingNav from '../components/LandingNav';
+import LeftPatch from '../components/LeftPatch';
 
 const Register = () => {
 return (
 <div className='mainContainer w-full h-[100vh]'>
     <LandingNav/>
+
+    
+    <div className="InnerContainer w-full h-auto md:px-20 p-5 flex flex-col md:flex-row justify-center items-center">
+        <LeftPatch/>
+
+        <div className="rightPatch p-5 w-[90%]  md:w-[30%] h-[400px] md:h-[550px]  rounded-xl  border-2 border-slate-400 flex justify-center items-center">
+        <div className="inputscontainer  w-[300px] h-full flex flex-col justify-center items-center gap-5">
+                <button className="bg-blue-700 text-white font-bold p-1 w-full h-[40px] rounded-lg flex justify-center items-center gap-10">
+                <img src='/icons/GIcon.png' className='h-[30px] w-[30px] rounded-md' alt='google'/>
+                <h2 className="signup font-sans">Signup with Google</h2>
+                </button>
+        
+                <h1 className='text-slate-500 font-bold'>OR</h1>
+        
+                <div className="inputs  text-black flex flex-col gap-5 w-full">
+                <input className='w-full h-[40px] p-5 rounded-xl  placeholder-slate-500 border-slate-300 border-2' placeholder='Fullname'/>
+                <input className='w-full h-[40px] p-5 rounded-xl  placeholder-slate-500 border-slate-300 border-2' placeholder='Email'/>
+                <input className='w-full h-[40px] p-5 rounded-xl  placeholder-slate-500 border-slate-300 border-2' placeholder='PhoneNumber'/>
+                <input className='w-full h-[40px] p-5 rounded-xl placeholder-slate-500  border-slate-300 border-2' placeholder='Password'/>
+                </div>
+                
+                <label className='font-sans text-sm'>By logging in, you are agreeing to TheKennel Club's <span className='underline text-blue-700 cursor-pointer'>Terms of Service</span> and <span className='underline text-blue-700 cursor-pointer'>privacy policy</span></label>
+        
+                <button className='mx-auto bg-purple-700 w-full h-[40px] rounded-lg text-white'>Get started for free</button>
+        
+                <div className="label text-sm">Already have an account? <span className='underline text-blue-700 cursor-pointer'><Link to="/register">Log In</Link></span></div>
+        </div>
+        </div>
+
+    </div>
 </div>
 )
 }
